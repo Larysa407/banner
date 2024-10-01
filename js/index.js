01112e55155d96a894f6d2a71e96137278a4d7d1
Name SurnameLarysa <lorik280319@gmail.com>
2024-10-01 14:44:23 +0300

    banner

---
+++ js/index.js
@@ -0,0 +1,8 @@
+let closeBtn = document.querySelector(".close-btn");
+let wrapper = document.querySelector(".wrapper");
+
+closeBtn.addEventListener("click", () => {
+    wrapper.classList.add('closed')
+});
+
+
